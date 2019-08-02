8ea155479e2df12b7a8a9f1cb22f052be692e462
kervensford <kervensford5@gmail.com>
2019-08-02 12:01:13 -0400

    prog2

---
+++ modulo_1/intro_javascript/ejercicios2_objectos/prog.js
@@ -0,0 +1,92 @@
+/*
+
+alert("suspann vole afe mounn");
+var nombre = prompt("Digita tu nombre");
+var edad = parseInt( prompt("Digita tu edad") );
+var ciudad = prompt("Digita tu ciudad");
+var hijos = parseInt( prompt("Digita la cantidad de hijos") );
+var casado= prompt("Digita si  esta casado");
+var carro = prompt("Digita si tu posee carro");
+*/
+
+var nombre = "kervens ford";
+var edad = 23;
+var ciudad = "cap haitien";
+var hijos = 2;
+var casado = true;
+var carro = false;
+
+console.log(nombre);
+console.log(edad);
+console.log(ciudad);
+console.log(hijos);
+console.log(casado);
+console.log(carro);
+
+
+var persona = {
+
+    nombre : "kervens ford",
+    edad : 23,
+    ciudad : "cap haitien",
+    hijos :2,
+    casado :true,
+    carro :false,
+}
+
+alert(persona.nombre);
+alert(persona.edad);
+alert(persona.ciudad);
+alert(persona.hijos);
+alert(persona.casado);
+alert(persona.carro);
+
+
+var estudiante = {
+
+    nombre: "kervens joseph",
+    carrera: "ing ",
+    matricula: 2171516,
+   cedula: 300,
+   telefono: 8094088298,
+  sizeRopa: "M"
+}
+alert(estudiante.nombre);
+alert(estudiante.carrera);
+alert(estudiante.matricula);
+alert(estudiante.cedula);
+alert(estudiante.telefono);
+alert(estudiante.sizeRopa);
+
+var perro = {
+
+    raza: "Wentchelle",
+    tamano: 1,
+    color: "gris",
+    nivelHambre: 100,
+    colorDientes: "amarillo",
+    nombre: "salit",
+    siTieneDueno:true,
+    anoDeNaciminto:10
+}
+
+alert(perro.raza);
+alert(perro.tamano);
+alert(perro.color);
+alert(perro.nivelHambre);
+alert(perro.colorDientes);
+alert(perro.nombre);
+alert(perro.siTieneDueno);
+alert(perro.anoDeNaciminto);
+
+var carro1= {
+
+    modelo: "sonata",
+    forma: "triangula",
+    tipoVelocidad: 6,
+    fabricante: "la nasa",
+    anosCreacion: 2019,
+    tipoMotor: "kaskooo",
+    color: "blue",
+    modo: "get out on my face"
+}
---
+++ modulo_1/intro_javascript/ejercicios2_objectos/sitio.js
@@ -0,0 +1,21 @@
+//Informacion del sitio web//
+
+var sitioWeb= {
+
+    titulo: "Los Caballeros",
+    subtitulo: "Diseno y Desarrollo web",
+    color: "Blue",
+    tieneLogo: true,
+    creador: " by kervens",
+    ano: 2019,
+    servicio: "livrazon a domicilio "
+    
+}
+
+alert(sitioWeb.titulo);
+alert(sitioWeb.subtitulo);
+alert(sitioWeb.color);
+alert(sitioWeb.tieneLogo);
+alert(sitioWeb.creador);
+alert(sitioWeb.ano);
+alert(sitioWeb.servicio);
