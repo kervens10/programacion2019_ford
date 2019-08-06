8d3346228f31c6e49ce9e9aaa946627a095392ea
kervens <kervensford5@gmail.com>
2019-08-06 09:55:31 -0400

    prog4

---
+++ modulo_1/intro_javascript/ejercicios3_estructuras_control/comparacion.js
@@ -0,0 +1,32 @@
+/*var ker = {
+    nombre: ford,
+     edad : 23,
+     estacasado:true
+}
+if (kerv) {
+    
+} else {
+    
+}
+*/
+/* Operaciones Aritmeticos //
+console.log(2 + 3 -1 );
+console.log(4 * 3 / 2);
+console.log(11 % 3 ** 2);
+*/
+var numero1  = parseInt ( prompt ("Digite 1 numeros "));
+var numero2 =  parseInt ( prompt("Digite 2 numeros "));
+
+var suma = (numero1) +(numero2);
+var resta = (numero1) - (numero2);
+var multiplicacion = (numero1) * (numero2);
+var division = (numero1) / (numero2);
+var residuo = (numero1) % (numero2);
+var potencia =  (numero1) ** (numero2);
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(residuo);
+console.log(potencia);
