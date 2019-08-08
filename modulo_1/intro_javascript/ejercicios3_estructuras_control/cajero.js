e9e0f2078d8a7e46cdf63cb62a3b2853af30af86
kervens <kervensford5@gmail.com>
2019-08-08 12:30:16 -0400

    prog7

---
+++ modulo_1/intro_javascript/ejercicios3_estructuras_control/cajero.js
@@ -0,0 +1,87 @@
+
+//0- seleccionar idioma //
+var idioma = prompt ("Seleccione  su  idioma");
+
+//1- Introducir tarjeta //
+var trajeta = prompt("Introduzca la tarjeta,por favor.");
+//valida que la longitud de digitos sea 12 //
+if (trajeta.length > 12) {
+    alert("La tarjeta no es valida");
+} else {
+    
+}
+
+//Valida que la longitud de digitos sea 4 //
+if (pin.length > 4 ) {
+    alert(" El pin  no es valida");
+} else {
+
+}
+
+//2- Digitar PIN//
+var pin = prompt(`Elija opcion a realizar
+1- Retiro
+2 -Avance
+3- Chequear Balance
+4- Deposito
+`);
+
+//4-seleccionar tipo de cuenta//
+var tipoCuenta =  prompt(`Seleccione tipos de cuenta
+1- Cuenta de ahorro
+2- Tarjeta de credito
+3- Cuenta coriente
+`);
+//5-mostrar opciones de monto//
+
+
+var opcionMonto = prompt(`Seleccione el tipos de monto
+1- 200$
+2- 1200$
+3- 1250$
+4- 2500$
+5- 3000$
+6- 5000$
+7- 7000$ 
+8- otro opciones 
+`);
+
+//7- Seleccionar el monto//
+
+
+
+//  8-mostrar opcion de recibo //
+var opcionRecibo = prompt(" ¿Desea un comprobante. ");
+// 9- Elegir Si o No el comprobante del recibo//
+if (true) {
+    alert("Imprimiendo el recibo...");
+}
+    
+
+//10- mostrar mesaje de retirar  dinero//
+alert(" Retira tu dinero ");
+
+//11- mostrar mesaje de retirar  trajeta//
+alert(" Retira tu trajeta " );
+
+
+
+
+
+
+
+
+/*
+
+
+alert ("Seleccionar las opciones !");
+ 
+var u = confirm("confirm valor ");
+console.log(u);
+
+if (u) {
+    console.log("retirar");
+} else {
+    console.log("salir");
+}
+*/
