442f49a319dd03e040d5567b4ff0b5d13710a6e7
kerve <kervensford5@gmail.com>
2019-08-09 12:22:49 -0400

    prog8

---
+++ modulo_1/intro_javascript/ejercicios4_relaciones/mesa.js
@@ -0,0 +1,15 @@
+var mesa = {
+    hayCuchara: false,
+    hayTenedor: false,
+
+}
+
+if (mesa.hayCuchara || mesa.hayTenedor) {
+    console.log("Manje ak dwet ou ");
+    document.write("snack");
+
+} else {
+    console.log("Al vole");
+    document.write("taste a good pussy");
+
+}
---
+++ modulo_1/intro_javascript/ejercicios4_relaciones/relacion.js
@@ -0,0 +1,15 @@
+var carro = {
+    tienebateria: true,
+    tengollave:false,
+
+}
+
+if (carro.tienebateria && carro.tengollave) {
+    console.log("El carro va a encender ");
+    document.write("snack");
+
+} else {
+    console.log("No enciende");
+      document.write("taste a good pussy");
+
+}
