e2af1d1432e7d6eca73e82dab9439fd1817e2635
kervensford5@gmail.com <kervensford5@gmail.com>
2019-10-15 12:13:53 -0400

    ker22

---
+++ Movimiento/Movimiento/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Movimiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(158, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.o);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(133, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Coordernadas";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(12, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(350, 401);
+            this.label3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Movimiento/Movimiento/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movimiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Obtener el valor de la tecla
+            var keyValue = e.KeyValue;
+
+            var posicionX = label1.Location.X;
+            var posicionY = label1.Location.Y;
+
+           
+
+            // MessageBox.Show( posicionX.ToString() );
+            // MessageBox.Show( posicionY.ToString() );
+
+            // label1.Location = new System.Drawing.Point(0, 0);
+
+
+
+            //Cambiar color 
+
+            if (keyValue == 67)
+            {
+                MessageBox.Show("Cambiando Color");
+
+                var random = new Random();
+
+                var numeroAleatorio = random.Next(0, 5);
+                MessageBox.Show(numeroAleatorio.ToString());
+    
+
+
+
+            //Listado de colores 
+
+        Color [] colores = {Color.Blue, Color.Red, Color.Green, Color.Peru, Color.PowderBlue, Color.PapayaWhip, Color.PaleTurquoise};
+
+
+        MessageBox.Show(colores[numeroAleatorio].ToString());
+
+
+        //label1.BackColor = colores[numeroAleatorio];
+
+
+
+        label1.BackColor = Color.FromArgb (255, 0, 0);
+
+
+
+
+            }
+
+
+
+            // Izquierda
+            if (keyValue == 37 || keyValue == 65)
+            {
+                var anchoPadre = label1.Width;
+                var anchoHijo = label2.Width;
+                var margen = label1.Location.X;
+
+                 var  CentroX = ((anchoPadre + anchoHijo) / 2) - margen;
+
+
+                if (posicionX <= 15)
+                {
+                    return;
+                }
+                // MessageBox.Show("Flecha a la izquierda");
+                label1.Location = new System.Drawing.Point(posicionX - 5, posicionY);
+                label2.Text = label1.Location.ToString();
+            }
+
+            // Derecha
+            if (keyValue == 39 || keyValue == 68)
+            {
+
+                if (posicionX >= 332)
+                {
+                    return;
+                }
+                // MessageBox.Show("Flecha a la derecha");
+                label1.Location = new System.Drawing.Point(posicionX + 5, posicionY);
+                label2.Text = label1.Location.ToString();
+            }
+
+            // Abajo
+            if (keyValue == 40 || keyValue == 83)
+            {
+
+                if (posicionY >= 409)
+                {
+                    return;
+                }
+                // MessageBox.Show("Flecha para abajo");
+                label1.Location = new System.Drawing.Point(posicionX, posicionY + 5);
+                label2.Text = label1.Location.ToString();
+            }
+
+            // Arriba
+            if (keyValue == 38 || keyValue == 87)
+            {
+
+                if (posicionY <= 39)
+                {
+                    return;
+                }
+                // MessageBox.Show("Flecha para arriba");
+                label1.Location = new System.Drawing.Point(posicionX, posicionY - 5);
+                label2.Text = label1.Location.ToString();
+            }
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void o(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+    }
+}
